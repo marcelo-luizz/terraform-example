adb66440aea4abdcfdb3d9bc67ebff8960e5785b
Marcelo Luiz <87508684+marcelo-luizz@users.noreply.github.com>
2026-05-04 23:08:10 -0300

    Comment out provider and bucket resource in main.tf
    Comment out Google Cloud provider and storage bucket resource.

--- environments/dev/team-test-/bucket/main.tf
+++ environments/dev/team-test-/bucket/main.tf
@@ -1,10 +1,10 @@
-provider "google" {
-  project = "devops-labs-397603"
-  region  = "us-central1"
-}
+#provider "google" {
+#  project = "devops-labs-397603"
+#  region  = "us-central1"
+#}
 
-resource "google_storage_bucket" "meu_bucket" {
-  name          = "nome-unico-do-meu-bucket-12345617"
-  location      = "US" 
-  force_destroy = true
-}
+#resource "google_storage_bucket" "meu_bucket" {
+#  name          = "nome-unico-do-meu-bucket-12345617"
+#  location      = "US" 
+#  force_destroy = true
+#}
