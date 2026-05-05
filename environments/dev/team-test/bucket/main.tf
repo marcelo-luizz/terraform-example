b24a8cdb1bd900d57e2198ba124dc6db698201a6
Marcelo Luiz <87508684+marcelo-luizz@users.noreply.github.com>
2026-05-04 21:29:04 -0300

    Add Google Cloud Storage bucket configuration

---
+++ environments/dev/team-test/bucket/main.tf
@@ -0,0 +1,10 @@
+provider "google" {
+  project = "devops-labs-397603"
+  region  = "us-central1"
+}
+
+resource "google_storage_bucket" "meu_bucket" {
+  name          = "nome-unico-do-meu-bucket-12345"
+  location      = "US" 
+  force_destroy = true
+}
