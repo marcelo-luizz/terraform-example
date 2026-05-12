36a16c0c6809604ad8601aa18eaedafae1f69837
Marcelo Luiz <87508684+marcelo-luizz@users.noreply.github.com>
2026-05-12 19:54:43 -0300

    Update outputs to handle multiple GCS buckets

--- terraform-modules/gcp/gcs/outputs.tf
+++ terraform-modules/gcp/gcs/outputs.tf
@@ -1,7 +1,7 @@
 output "bucket_name" {
-  value = google_storage_bucket.this.name
+  value = google_storage_bucket.this[*].name
 }
 
 output "bucket_url" {
-  value = google_storage_bucket.this.url
+  value = google_storage_bucket.this[*].url
 }
